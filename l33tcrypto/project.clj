96b090f882d0d4fa184caa3fda7d614e02695ea2
Mitchell Pavel <mitchell.pavel@hotmail.com>
2020-02-15 18:03:43 -0600

    add clojure

---
+++ l33tcrypto/project.clj
@@ -0,0 +1,8 @@
+(defproject l33tcrypto "0.0.0-SNAPSHOT"
+  :description "recursively base64 encode strings for variable security"
+  :url "https://github.com/circa10a/l33tcrypto"
+  :license {:name "MIT License"
+            :url "https://github.com/circa10a/l33tcrypto/blob/master/LICENSE"}
+  :dependencies [[org.clojure/clojure "1.10.0"]]
+  :repl-options {:init-ns l33tcrypto.core}
+  :main main)
